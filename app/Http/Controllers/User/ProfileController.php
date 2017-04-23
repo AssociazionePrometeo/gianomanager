efa848821185cf8b7fba68e3fe66adb02f049bdc
Giuseppe Treccarichi <giutrec@gmail.com>
2017-04-23 18:03:08 +0200

    add row user profile page and corrected a /home route

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    $user = User::all()->except(Auth::id());
+
+    return view('user.profile', compact('user'));
+    }
+
+     /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+}
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,20 +2,20 @@
 <html>
 
     <head>
-        <title>@yield('title', 'S19') @yield('title.suffix')</title>
+        <title>@yield('title', 'Giano') @yield('title.suffix')</title>
         <link rel="stylesheet" href="/assets/css/style.css">
     </head>
 
-    <body class="@yield('class')">            
+    <body class="@yield('class')">
 
         @yield('navigation')
 
         <main id="main" class="container">
-    
+
             @yield('main')
-            
+
         </main>
-        
+
         @section('javascripts')
             <script src="{{ asset('assets/js/main.min.js') }}"></script>
             <script>require(['jquery', 'selectize'], function($) { $('select').selectize(); })</script>

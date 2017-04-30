4a58087d865a67b9d37f05775c140528ffa5edcf
mattbit <me@mattbit.com>
2017-04-30 13:07:39 +0200

    Add flash messages

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -10,6 +10,7 @@
             <li><a href="{{ route('admin.reservations.index') }}">Prenotazioni</a></li>
             <li><a href="{{ route('admin.cards.index') }}">Tessere</a></li>
             <li><a href="{{ route('admin.users.index') }}">Utenti</a></li>
+            <li><a href="{{ route('admin.roles.index') }}">Ruoli</a></li>
         </ul>
         <ul class="secondary">
           <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci</a></li>

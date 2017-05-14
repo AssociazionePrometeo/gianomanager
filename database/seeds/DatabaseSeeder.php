2bca500a2244684816ccd8d4280beab2652b09c5
mattbit <me@mattbit.com>
2017-05-14 13:35:33 +0200

    Fix database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Role;
 use App\User;
+use App\Permission;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Create default roles
-        $admin = new Role(['id' => 'admin', 'name' => 'Administrators', 'permissions' => '{"card-view":true,"card-create":true,"card-update":true,"card-delete":true,"resource-view":true,"resource-create":true,"resource-update":true,"resource-delete":true,"reservation-view":true,"reservation-create":true,"reservation-update":true,"reservation-delete":true,"user-view":true,"user-create":true,"user-update":true,"user-delete":true,"role-view":true,"role-create":true,"role-update":true,"role-delete":true}']);
+        $admin = new Role(['id' => 'admin', 'name' => 'Administrators']);
+        $admin->permissions = Permission::getAdminValues();
         $admin->protected = true;
-        $admin->permissions = ['admin' => true];
         $admin->save();
 
         $default = new Role(['id' => 'default', 'name' => 'Default']);
